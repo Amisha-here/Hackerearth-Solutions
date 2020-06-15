bbe58202942e44139ba136bafd2d7e068188d6df
Amisha Purswani <ameeshapurswani@gmail.com>
2020-06-16 01:04:13 +0530

    Create Life_the_Universe_and_Everything.cpp

---
+++ Basic Programming/Basics of Input_Output/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ int a[1100000];
+
+ for(int i=0;i<100000000;i++)
+ {
+     cin>>a[i];
+     if (a[i]==42)
+     break;
+     cout<<a[i]<<endl;
+ }
+   
+
+}
