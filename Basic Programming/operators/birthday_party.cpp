8237795ce6b59ea7db0d6848120823dcf965e7be
Amisha Purswani <ameeshapurswani@gmail.com>
2020-06-18 01:25:36 +0530

    Create birthday_party.cpp

---
+++ Basic Programming/operators/birthday_party.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int t;
+   int n,m;
+   cin>>t;
+
+   while(t--)
+   {
+     cin>>n>>m;
+     if(m%n==0)
+     cout<<"Yes"<<endl;
+     else
+     cout<<"No"<<endl;
+
+   }
+
+
+}
