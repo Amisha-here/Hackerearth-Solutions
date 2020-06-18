367722d36ff508df3961b652621b8c009bfc8ffa
Amisha Purswani <ameeshapurswani@gmail.com>
2020-06-19 00:03:30 +0530

    Create max_profit.c

---
+++ Basic Programming/basics_of_implementation/max_profit.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   int arr[n];
+
+   for(int i=0;i<n;i++)
+   {
+     scanf("%d",&arr[i]);
+    }
+
+    int max=0;
+   
+   for(int i=0;i<n;i++)
+    {
+      for(int j=i+1;j<n;j++)
+        {
+        if(arr[i]>arr[j])
+        continue;
+
+        else
+         {
+            if(arr[j]-arr[i]>max)
+             max=arr[j]-arr[i];
+        }
+           }
+     }
+printf("%d",max);
+}
