5e642a0fdfdb028e0bac6781605af33e081b03ce
Amisha Purswani <ameeshapurswani@gmail.com>
2020-06-22 23:16:09 +0530

    Create A+B.cpp

---
+++ Basic Programming/complexity_analysis/A+B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace std;
+using boost::multiprecision::cpp_int;
+
+int main()
+{
+    cpp_int a,b;
+    while(cin>>a>>b)     
+        cout<<a+b<<endl; 
+    
+}
+             
